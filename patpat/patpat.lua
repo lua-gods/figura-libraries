a1cbc67d2e527c51ee229476fdbf4fff03bf052d
Bitslayn <96226175+Bitslayn@users.noreply.github.com>
2025-04-04 11:38:56 -0400

    Add support for foxpat heart cancelling

--- patpat/patpat.lua
+++ patpat/patpat.lua
@@ -180,6 +180,7 @@ end
 function pings.patpat(a, b, c)
    if not player:isLoaded() then return end
    local avatarVars, pos, boundingBox, pattingOutput
+   local ret
    if b then -- block
       -- decrypt position
       local receivedPos = vec(a, b, c)
@@ -194,7 +195,7 @@ function pings.patpat(a, b, c)
       pos = blockPos + vec(0.5, 0, 0.5)
       boundingBox = vec(0.8, 0.8, 0.8)
       -- call petpet function
-      pcall(avatarVars["petpet.playerHead"], myUuid, config.holdFor, blockPos.x, blockPos.y, blockPos.z)
+      _, ret = pcall(avatarVars["petpet.playerHead"], myUuid, config.holdFor, blockPos.x, blockPos.y, blockPos.z)
       pattingOutput = callEvent("head", "patting", blockPos)
    else -- entity
       local entity = world.getEntity(unpackUuid(a))
@@ -208,9 +209,10 @@ function pings.patpat(a, b, c)
          boundingBox = entity:getBoundingBox()
       end
       -- call petpet function
-      pcall(avatarVars["petpet"], myUuid, config.holdFor)
+      _, ret = pcall(avatarVars["petpet"], myUuid, config.holdFor)
       pattingOutput = callEvent("player", "patting", entity)
    end
+   local cancelHearts = type(ret) == "table" and ret[2] or avatarVars['patpat.noHearts'] or ret
    -- cancel patpat particles when returned true in patting event
    for _, v in pairs(pattingOutput) do
       if v[1] then
@@ -218,7 +220,7 @@ function pings.patpat(a, b, c)
       end
    end
    -- spawn particles
-   if avatarVars['patpat.noHearts'] then return end
+   if not cancelHearts then return end
    pos = pos - boundingBox.x_z * 0.5 + vec(
       math.random(),
       math.random(),
