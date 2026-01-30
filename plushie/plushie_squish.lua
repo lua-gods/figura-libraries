0fdc855bbd548925b7e59572c1a765371199c291
auriafoxgirl <53555592+auriafoxgirl@users.noreply.github.com>
2026-01-30 14:48:27 +0100

    plushie squish variant

--- plushie/plushie_squish.lua
+++ plushie/plushie_squish.lua
@@ -9,6 +9,8 @@ sit on stairs - makes plushie sit on stairs
 
 there might be issues with stacking if pivot point of model group is not at 0 0 0
 
+this variant allows plushies to be squished, requires patpat script
+
 ]]-- config
 local model = models.plushie.Skull -- model part for plushie
 local headOffset = 6 -- how high should plushie move when entity have it on helmet slot
@@ -19,6 +21,18 @@ local offset = vec(0, 1, 0)
 local vec3 = vec(1, 1, 1)
 local vec2Half = vec(0.5, 0.5)
 local myUuid = avatar:getUUID()
+
+local tickTimer = 0
+local squishData = {}
+local squishDataI = nil
+
+-- patpat squish
+local patpat = require("patpat") -- path to patpat script
+
+table.insert(patpat.head.oncePat, function(_, pos)
+   squishData[tostring(pos)] = tickTimer
+   sounds['entity.axolotl.idle_air']:pos(pos + 0.5):pitch(2):play()
+end)
 
 -- check for head
 local function myHead(bl)
@@ -49,7 +63,21 @@ function events.skull_render(delta, block, item, entity, mode)
    while myHead(world.getBlockState(pos + offset * size)) do
       size = size + 1
    end
-   model:setScale(vec3 * size)
+   -- squish
+   local squish = vec(1, 1, 1)
+   local posStr = tostring(pos)
+   if squishData[posStr] then
+      local squishTime = (tickTimer + delta - squishData[posStr]) / 8
+      if squishTime > 1 then
+         squishTime = 1
+         squishData[posStr] = nil
+      end
+      squish.y = 1 - math.sin(squishTime * 9.5) * 2 ^ (-9 * squishTime) * 0.9
+      squish.x = 1 / squish.y
+      squish.z = squish.x
+   end
+   -- scale
+   model:setScale(squish * size)
    -- move to floor
    if block.id ~= "minecraft:player_head" then
       model:setPos(0, 0, 0)
@@ -70,5 +98,19 @@ function events.skull_render(delta, block, item, entity, mode)
       model:setPos(0, y * 16 - 16, 0)
    else
       model:setPos(0, 0, 0)
+   end
+end
+
+-- clean up squish data
+function events.world_tick()
+   tickTimer = tickTimer + 1
+   local timeLimit = tickTimer - 10 -- limit before it is removed in ticks
+   for _ = 1, 2 do
+     squishDataI = next(squishData, squishData[squishDataI] and squishDataI or nil)
+     if squishDataI then
+       if squishData[squishDataI] < timeLimit then
+         squishData[squishDataI] = nil
+       end
+     end
    end
 end
