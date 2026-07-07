293ab632bc3d54206cb8c09c80774b061891cd07
auriafoxgirl <53555592+auriafoxgirl@users.noreply.github.com>
2026-07-07 13:40:03 +0200

    awa

--- bendlib/main.lua
+++ bendlib/main.lua
@@ -1,7 +1,20 @@
 vanilla_model.PLAYER:setVisible(false)
 vanilla_model.ARMOR:setVisible(false)
 
+local bendLib = require("bend")
+
 function events.render()
    local rot = vanilla_model.HEAD:getOriginRot()
    models.model.group.cube:setRot(rot)
+end
+
+local model = models.model.Skull
+model:setScale(0.5)
+for i = 1, 8 do
+   model = model["part"..i]
+
+   bendLib.new(model)
+
+   local y = 180/8
+   model:setRot(0, y, 0)
 end
